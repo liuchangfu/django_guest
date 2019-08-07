644d9d970daea86f7768fa2bb9aec0e408ab9af5
空空如也 <shift_1220@163.com>
2019-08-07 18:39:50 +0800

    增加后台显示字段，搜索和过滤字段

--- sign/admin.py
+++ sign/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from sign.models import Event, Guest
 
+
 # Register your models here.
 
 
-admin.site.register(Event)
-admin.site.register(Guest)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'start_time']
+    list_filter = ['name']
+    search_fields = ['status']
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time']
+    list_filter = ['sign']
+    search_fields = ['realname', 'phone']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
 
 admin.site.site_header = '签到管理系统'
 admin.site.site_title = '签到管理系统'
